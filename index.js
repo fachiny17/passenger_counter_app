019ceac740e06f7842d0764dc0828032c860c19a
IHEANYI, FAVOUR CHISOM <iheanyifavour.ube3room312@gmail.com>
2023-03-17 23:30:51 +0100

    made an update

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-// document.getElementById("count-el").innerText = 5
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
 let count = 0
+
+console.log(saveEl)
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+// Create a function, save(), which logs out the count when it's called
+function save() {
+    let countStr = count + "-"
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
